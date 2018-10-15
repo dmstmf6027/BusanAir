2eb24c3bfcace961af42ded42908fdda11f4bc6d
D7703_08 <d7703_08@d7703-08ui-imac.local>
2018-10-15 15:08:23 +0900

    123

---
+++ BusanAir/AirData.swift
@@ -0,0 +1,15 @@
+//
+//  AirData.swift
+//  BusanAir
+//
+//  Created by D7703_08 on 2018. 10. 15..
+//  Copyright © 2018년 pgm. All rights reserved.
+//
+
+import Foundation
+
+class AirData{
+    var dPm10 = ""
+    var dSite = ""
+    var dPm10Cai = ""
+}
