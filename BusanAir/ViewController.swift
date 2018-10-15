56a5d4b57235eff033d8ccebd536a3104a611a8f
D7703_08 <d7703_08@d7703-08ui-imac.local>
2018-10-15 14:31:45 +0900

    Initial Commit

---
+++ BusanAir/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BusanAir
+//
+//  Created by D7703_08 on 2018. 10. 15..
+//  Copyright © 2018년 pgm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
